bec04c06e8e71a529462fe0c40359555ced70c2a
huzaifa <mohdhuzaifa359@gmail.com>
2023-02-15 01:52:02 +0530

    ok

--- src/components/FirstSection/index.js
+++ src/components/FirstSection/index.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { Box, Text, Heading, Button } from "@chakra-ui/react";
+import { Box, Text, Heading, Button, chakra } from "@chakra-ui/react";
 import Image from "next/image";
+const Cimage = chakra(Image, {
+  shouldForwardProp: (prop) => ["width", "height", "src", "alt"],
+});
 const outerBox = {
   width: "100vw",
   height: "100vh",
@@ -18,11 +21,11 @@ const leftPane = {
 };
 const rightPane = {
   height: "100%",
-  background: "black",
-  width: "50%",
+  position: "relative",
+  flexGrow: "1",
 };
 const head = {
-  marginTop: "112px",
+  marginTop: "111px",
   fontSize: "3.5em",
   fontWeight: "700",
   lineHeight: "69px",
@@ -54,38 +57,143 @@ const exploreButton = {
   background: "#E23744",
   color: "#ffffff",
 };
+const pizzaImage = {
+  position: "absolute",
+  top: "0px",
+  right: "0px",
+  display: "inline-block",
+};
+const vector = {
+  position: "absolute",
+  top: "0px",
+  right: "0px",
+  display: "inline-block",
+};
+const touchButton = {
+  position: "absolute",
+  top: "32px",
+  right: "41px",
+  background: "transparent",
+  height: "42px",
+  width: "122px",
+  borderRadius: "21px",
+  fontSize: "1em",
+  lineHeight: "36px",
+  color: "#ffffff",
+  border: "1px solid #ffffff",
+};
+const secondSection = {
+  height: "646px",
+  width: "100%",
+  background: "red",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+};
+const innerSecondSection = {
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-evenly",
+};
+const testBox = {
+  width: "447px",
+  height: "254px",
+  background: "yellow",
+  marginX: "2px",
+};
+const aboutHeading = {
+  fontWeight: "600",
+  fontSize: "45px",
+  lineHeight: "27px",
+};
+const aboutInfo = {
+  fontSize: "15px",
+  lineHeight: "27px",
+  marginTop: "27.42px",
+};
+const aboutButton = {
+  marginTop: "21px",
+  fontWeight: "600",
+  height: "42px",
+  lineHeight: "36px",
+  background: "red",
+  fontSize: "16px",
+  paddingX: "1.5em",
+  borderRadius: "31px",
+  width: "133px",
+  marginLeft: "-1px",
+  background: "#E23744",
+  color: "#ffffff",
+  letterSpacing: "3%",
+};
 function index() {
   return (
-    <Box sx={outerBox}>
-      <Box sx={innerBox}>
-        <Box sx={leftPane}>
-          <Image src="/logo.png" width={107} height={83} alt="logo"></Image>
+    <>
+      <Box sx={outerBox}>
+        <Box sx={innerBox}>
+          <Box sx={leftPane}>
+            <Image src="/logo.png" width={107} height={83} alt="logo"></Image>
 
-          <Heading sx={head}>
-            <Text as="span" color="#0E2368">
-              {" "}
-              Discover the <br />{" "}
+            <Heading sx={head}>
+              <Text as="span" color="#0E2368">
+                {" "}
+                Discover the <br />{" "}
+              </Text>
+              <Text as="span" color="#E23744">
+                Best
+              </Text>{" "}
+              <Text as="span" color="#0E2368">
+                Food
+              </Text>{" "}
+              <br />
+              and Drinks
+            </Heading>
+            <Text sx={subheading}>
+              Naturally made Healthcare Products for the <br />
+              better care & support for your body
             </Text>
-            <Text as="span" color="#E23744">
-              Best
-            </Text>{" "}
-            <Text as="span" color="#0E2368">
-              Food
-            </Text>{" "}
-            <br />
-            and Drinks
-          </Heading>
-          <Text sx={subheading}>
-            Naturally made Healthcare Products for the <br />
-            better care & support for your body
-          </Text>
-          <Button sx={exploreButton}>Explore Now!</Button>
-        </Box>
-        <Box sx={rightPane}>
-          <Image position="relative" src="/pizza.png" width={735} height={705} alt="pizza"></Image>
+            <Button sx={exploreButton}>Explore Now!</Button>
+          </Box>
+          <Box sx={rightPane}>
+            <Cimage
+              sx={pizzaImage}
+              src="/pizza.png"
+              width={735}
+              height={804}
+              alt="pizza"
+            ></Cimage>
+            <Cimage
+              sx={vector}
+              src="/vector.svg"
+              width={752}
+              height={839}
+              alt="pizza"
+            ></Cimage>
+            <Button sx={touchButton}>Get804 In Touch</Button>
+          </Box>
         </Box>
       </Box>
-    </Box>
+      <Box sx={secondSection}>
+        <Box sx={innerSecondSection}>
+          <Image src="/second.png" width={384} height={468} alt="foodImage" />
+          <Box sx={testBox}>
+            <Heading sx={aboutHeading}>About Us</Heading>
+            <Text sx={aboutInfo}>
+              Lorem Ipsum is simply dummy text of the printing and
+              <br /> typesetting industry. Lorem Ipsum has been the industry's{" "}
+              <br />
+              standard dummy text ever since the 1500s, when an unknown <br />{" "}
+              printer took a galley of type and scrambled it to make a type{" "}
+              <br />
+              specimen book. t has survived not only five centuries.
+            </Text>
+            <Button sx={aboutButton}>Read More</Button>
+          </Box>
+        </Box>
+      </Box>
+    </>
   );
 }
 
